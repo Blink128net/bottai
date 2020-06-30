7ca6ca6b9f198ed366157f7acbb652840a716424
Bots.Business <git.sync@bots.business>
2020-06-30 19:31:56 +0000

    Bots.Business export for bot id: 107267

--- commands/_debug.js
+++ commands/_debug.js
@@ -13,5 +13,3 @@ let apikey = Libs.BlockIO.setSecretPin("52084242");
 
 let coin= Libs.BlockIO.Dogecoin.setApiKey("20a2-2031-8bc0-7fbf");
 Bot.sendMessage("Api Key for " + coin + " was stored");
-
-
